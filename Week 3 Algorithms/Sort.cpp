e4b3595fb90e5e5612034d02118cf6e5c64b10ec
Zeeshan Ahmad <zeeshan.techgeek123@gmail.com>
2025-09-21 09:58:16 +0500

    Add files via upload

---
+++ Week 3 Algorithms/Sort.cpp
@@ -0,0 +1,11 @@
+sort1 uses: Bubble Sort
+
+How do you know?: It takes less time for sorted list, and more time for random list
+
+sort2 uses: Merge Sort
+
+How do you know?: It takes almost the same (and less) time for both random and sorted lists
+
+sort3 uses: Selection Sort
+
+How do you know?: It takes almost the same (and more) time for sorted and random lists
